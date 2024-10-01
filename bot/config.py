40df396475051c59ad17121328293c3209fc90bd
ifanfomin <ifanfomin@rambler.ru>
2024-10-01 22:55:19 +0300

    Добавил пустой config.py

---
+++ bot/config.py
@@ -0,0 +1,12 @@
+class Config:
+    def __init__(self):
+        self.BOT_TOKEN = ""
+        self.admin_list = []
+        self.assistants_list = []
+        self.SUPER_USERS = []
+        self.DB_HOST = ""
+        self.DB_USER = ""
+        self.DB_PASS = ""
+        self.DB_NAME = ""
+
+config = Config()
