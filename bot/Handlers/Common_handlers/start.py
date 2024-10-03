def6ecdcf6e4282e93249dd9b297f12d0a7205c9
ifanfomin <ifanfomin@rambler.ru>
2024-10-03 19:18:02 +0300

    В стартовом хендлере сделал вызов функции из класса для БД для добавления юзера если его там нет

--- bot/Handlers/Common_handlers/start.py
+++ bot/Handlers/Common_handlers/start.py
@@ -2,25 +2,39 @@ from aiogram import types
 from aiogram.dispatcher import FSMContext
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-from bot.Services.Database.connecting import conn, cursor
-from bot.bot import config_dict
+from bot.Utils.Database.requests import Database
+from bot.config import config
 
 
-async def cmd_start(message: types.Message, state: FSMContext):
+async def cmd_start(message: types.Message, role: str, db: Database , state: FSMContext):
     await state.finish()
 
     user_id = message.from_user.id
     user_full_name = message.from_user.full_name
     user_name = message.from_user.username
-    cursor.execute("SELECT * FROM users WHERE user_id=%s", (user_id,))
-    existing_user = cursor.fetchone()
 
-    if not existing_user:
-        cursor.execute("INSERT INTO users (user_id, username, name) VALUES (%s, %s, %s)",
-                       (user_id, user_name, user_full_name))
-        conn.commit()
+    print(role)
 
-    if user_id in config_dict["ADMINS"]:
+    print(db)
+
+    await db.add_user(
+        user_id=str(user_id),
+        username=user_name,
+        name=user_full_name
+    )
+
+    await message.answer("Привет, я вижу ты хочешь поскорее посмотреть игры, если так, то скорее выбирай одну из опций ниже:")
+
+
+    # cursor.execute("SELECT * FROM users WHERE user_id=%s", (user_id,))
+    # existing_user = cursor.fetchone()
+
+    # if not existing_user:
+    #     cursor.execute("INSERT INTO users (user_id, username, name) VALUES (%s, %s, %s)",
+    #                    (user_id, user_name, user_full_name))
+    #     conn.commit()
+
+    # if user_id in config_dict["ADMINS"]:
 
 
 
